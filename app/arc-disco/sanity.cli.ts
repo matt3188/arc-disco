5331dd3c6ac1a3dede8e7a8949e5384318b67f3e
Matt Coleman <matt.coleman@protonmail.com>
2024-02-11 19:27:08 +0000

    added new project

---
+++ app/arc-disco/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '1b16uatj',
+    dataset: 'production'
+  }
+})
---
+++ app/arc-disco/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'ARC Disco',
+
+  projectId: '1b16uatj',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
